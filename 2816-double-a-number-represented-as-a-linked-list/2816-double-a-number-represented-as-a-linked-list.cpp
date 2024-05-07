7caf16329aa38c69ab0b54aba4767a59edc9bfad
Mohamed Amine Jmal <102758258+me9ded@users.noreply.github.com>
2024-05-07 15:43:33 -0400

    Time: 169 ms (72.81%), Space: 120.7 MB (72.46%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        if(head->val>4){
+            head=new ListNode(0,head);
+        }
+        ListNode* node=head;
+        while(node!=NULL){
+            node->val=(node->val*2)%10;
+            if(node->next && node->next->val>4){
+                node->val+=1;
+            }
+            node=node->next;
+        }
+        return head;
+
+    }
+};
