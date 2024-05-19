430fe5b8290f71d6a4203d29e16634dcf5601fdf
Mohamed Amine Jmal <102758258+me9ded@users.noreply.github.com>
2024-05-18 21:42:12 -0400

    Time: 961 ms (59.09%), Space: 28 MB (84.66%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        max_sum=sum(max(n,k^n) for n in nums)
+        count=sum((n^k) > n for n in nums)
+        return max_sum-(min(abs(n-(n^k)) for n in nums) if count%2 else 0)
+        
