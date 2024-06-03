e17a87e3f060703da7da5da3cb7d2b95e5bc666b
Mohamed Amine Jmal <102758258+me9ded@users.noreply.github.com>
2024-06-03 18:22:34 -0400

    Time: 54 ms (80.16%), Space: 17.4 MB (99.51%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        m=len(s)
+        n=len(t)
+        i,j=0,0
+        while i<m and j<n:
+            if s[i]==t[j]:
+                j+=1
+            i+=1
+        return n-j
+
+        
