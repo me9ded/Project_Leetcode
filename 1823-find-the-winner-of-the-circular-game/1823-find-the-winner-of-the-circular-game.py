72c402db3653c9e56551ef29baacf35e491024e5
Mohamed Amine Jmal <102758258+me9ded@users.noreply.github.com>
2024-07-08 14:37:43 -0400

    Time: 46 ms (37.80%), Space: 16.5 MB (49.47%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,9 +1,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        ans=0
-        for i in range(2,n+1):
-            ans=(ans+k)%i
-        return ans+1
-            
+        temp=list(range(1,n+1))
+        start=0
+        while len(temp)>1:
+            value_to_remove=(start+k-1)%len(temp)
+            temp.pop(value_to_remove)
+            start=value_to_remove
+        return temp[0]
 
         
